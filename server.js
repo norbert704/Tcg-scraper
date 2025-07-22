83d8106b4edc1eea50530cdcf3f31e0602650b0e
norbert704 <norbert704@gmail.com>
2025-07-22 23:44:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const puppeteer = require(' 'puppeteer');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.get('/count', async (req, res) => {
